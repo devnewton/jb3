4877a523ccd8c52fc77b503f4ee812dbccfb5d4d
devnewton <devnewton@bci.im>
2020-04-29 08:05:48 +0200

    better preview

--- src/main/java/im/bci/jb3/preview/PreviewApiController.java
+++ src/main/java/im/bci/jb3/preview/PreviewApiController.java
@@ -7,8 +7,8 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.safety.Whitelist;
-import org.jsoup.select.Elements;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,38 +20,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/preview")
 public class PreviewApiController {
 
-	private final Log LOGGER = LogFactory.getLog(this.getClass());
+    private final Log LOGGER = LogFactory.getLog(this.getClass());
 
-	@RequestMapping(value = { "", "/" }, method = RequestMethod.GET)
-	public Preview url(@RequestParam(name = "url", required = true) String url, Model model) {
-		Preview preview = new Preview();
-		try {
-			Document doc = Jsoup.connect(url).get();
-			extractTitle(preview, doc);
-			extractImage(preview, doc);
-		} catch (IOException e) {
-			LOGGER.warn("Cannot preview url " + url, e);
-		}
-		return preview;
-	}
+    @RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
+    public Preview url(@RequestParam(name = "url", required = true) String url, Model model) {
+        Preview preview = new Preview();
+        try {
+            Document doc = Jsoup.connect(url).get();
+            extractTitle(preview, doc);
+            extractImage(preview, doc);
+        } catch (IOException e) {
+            preview.setTitle(url);
+            LOGGER.warn("Cannot preview url " + url, e);
+        }
+        return preview;
+    }
 
-	private void extractImage(Preview preview, Document doc) {
-		Elements metaOgImage = doc.select("meta[property=og:image]");
-		if (metaOgImage != null) {
-			String image = metaOgImage.attr("content");
-			image = Jsoup.clean(image, Whitelist.none());
-			if (StringUtils.isNotBlank(image)) {
-				preview.setImage(image);
-			}
-		}
-	}
+    private void extractImage(Preview preview, Document doc) {
+        Element metaOgImage = doc.selectFirst("meta[property=og:image]");
+        if (metaOgImage != null) {
+            String image = metaOgImage.absUrl("content");
+            image = Jsoup.clean(image, Whitelist.none());
+            if (StringUtils.isNotBlank(image)) {
+                preview.setImage(image);
+            }
+        }
+    }
 
-	private void extractTitle(Preview preview, Document doc) {
-		Elements metaOgTitle = doc.select("meta[property=og:title]");
-		String title = null != metaOgTitle ? metaOgTitle.attr("content") : doc.title();
-		if (StringUtils.isNotBlank(title)) {
-			preview.setTitle(Jsoup.clean(title, Whitelist.none()));
-		}
-	}
+    private void extractTitle(Preview preview, Document doc) {
+        Element metaOgTitle = doc.selectFirst("meta[property=og:title]");
+        String title = StringUtils.firstNonBlank(null != metaOgTitle ? metaOgTitle.attr("content") : null, doc.title(), doc.baseUri());
+        preview.setTitle(Jsoup.clean(title, Whitelist.none()));
+    }
 
 }
